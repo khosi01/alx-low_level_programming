aa8e58e57fb93b51dc69a38e95dc63b39a71986f
Khosi <Khosi>
2023-03-23 12:13:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,8 +1,9 @@
+#include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @C: The character to print
+ *
  * Return: 0n success 1.
  * 0n error, -1 is returned, and error is set appropriatley.
  */
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 void print_alpabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
+#endif
