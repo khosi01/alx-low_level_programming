4e02bb9ad2ceb8199c10e11162c171b69ef10adb
Khosi <Khosi>
2023-03-25 14:04:47 -0700

    3-print-aphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return alphabets both lower and upper cases
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+	return (0);
+}
